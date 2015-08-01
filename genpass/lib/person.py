4558bea1c05720bd9dc675e3e66dce34ffe5f2c2
Ricter Z <ricterzheng@gmail.com>
2015-08-01 11:00:57 +0800

    add commment of methods of `Person` class

--- genpass/lib/person.py
+++ genpass/lib/person.py
@@ -36,6 +36,40 @@ class Person(object):
         return result
 
     def generate_source_dict(self):
+        '''generate source dictionary `source_dict`.
+        `source_dict` is a dictionary which the value contains all
+        the situation of provided.
+        The map of generate `source_dict` named `field_map`.
+        `field_map` is tuple.
+
+        ```
+        field_map = (
+            ('test', None),
+            (('test1', 'test'), built_in.generate_formats),
+            ('test2', built_in.generate_formats, generate.custom_function),
+        )
+        ```
+        The first element of tuple is the field name, and if the first element
+        is tuple, then the first element of the tuple is the field name from
+        user information, the second element is a alias name in the `source_dict`.
+
+        The second element of tuple is the rules for formatting and transferring
+        the data which user provided, which is a sequence of lambda.
+
+        The third element of tuple is the custom formatting function. The function
+        has 2 parameters. First of the parameters is the data of user information,
+        and the type is a list. The second of the parameters is the rule. The function
+        returned a set of password fragment.
+
+        A sample of custom formatting function is:
+
+        ```
+        def custom_generator(data, rule):
+            return set(data)
+        ```
+
+        :return: None
+        '''
         source_dict = {}
         for row in self.field_map:
             if len(row) == 3:
@@ -75,13 +109,21 @@ class Person(object):
                 self.source_dict[key] = value
 
     def combined_zip(self, dependence):
+        '''A generator yield passwords by combining the password fragment.
+
+        `dependence` is a list of field. The function will combine the values of
+        each key in the `source_dict`, while the key provided by `dependence`.
+
+        :param dependence:
+        :return:
+        '''
         for res in (zip(dependence.keys(), i) for i in product(*dependence.itervalues())):
             yield dict(res)
 
     def generate_password(self):
-        '''generate passwords
+        '''A generator yield passwords
 
-        :return: list
+        :return:
         '''
         self.generate_source_dict()
         match_keys = re.compile('\{(%s)\}' % '|'.join(self.information.keys()))
@@ -93,10 +135,6 @@ class Person(object):
                     yield rule.format(**i)
 
     def generate_password_with_dict(self):
-        '''generate passwords with wake passwords dict
-
-        :return: string list
-        '''
         pass
 
     def __str__(self):
