b0f895df07ae7bab44353c3906ba4517906bfaf0
RicterZ <ricterzheng@gmail.com>
2019-11-20 12:58:52 +0800

    rm

--- genpass/config.py
+++
@@ -1,6 +0,0 @@
-# coding: utf-8
-import os
-from genpass.lib.constants import BASE_DIR
-
-DICT = os.path.join(BASE_DIR, './data/dict.txt')
-PINYIN = os.path.join(BASE_DIR, './data/word.data')
--- genpass/lib/constants.py
+++
@@ -1,8 +0,0 @@
-# coding: utf-8
-import os
-
-# basedir of genpass
-BASE_DIR = os.path.join(os.path.dirname(__file__), '../')
-
-# sequences of python
-SEQUENCES = (tuple, list, set)
